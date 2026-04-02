ab352d79ffce8de353c83dfa01f9c5d17398604a
s6655806 <salahhassn232@gmail.com>
2026-04-02 08:33:19 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+import telebot
+from threading import Thread
+
+app = Flask(__name__)
+
+# الصفحة الرئيسية عشان الرابط يشتغل في MoboEasy
+@app.route('/')
+def home():
+    return "<h1>Carlos Bot is Online!</h1>"
+
+# التوكن الخاص بك
+TOKEN = "8754467241:AAHTlQY26Nn0keE4B9H-SVLRCD1G
